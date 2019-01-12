16a6181583aa4082e5137b5cdfab4069040e2277
Chuang Yu Min <chg.yumin@gmail.com>
2019-01-12 00:16:59 -0300

    Initialized project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wedding_gallery',
+    packages=['wedding_gallery'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
