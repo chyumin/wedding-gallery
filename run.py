ebdc93858cd8177a7a0fe80dd453d28542f63f2f
Chuang Yu Min <chg.yumin@gmail.com>
2019-01-13 13:19:54 -0300

    Added migrations, default db path to home

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
 from wedding_gallery import app
 
+manager = Manager(app)
+
+
+@manager.option('-h', '--host', dest='host', default='0.0.0.0')
+@manager.option('-p', '--port', dest='port', type=int, default=5000)
+def server(host, port):
+    app.run(host=host, port=port, threaded=True)
+
+
 if __name__ == '__main__':
-    app.run()
+    manager.add_command('db', MigrateCommand)
+    manager.run()
