3c9793fbb0027d33d69b635a9740eaad1cacc960
Chuang Yu Min <chg.yumin@gmail.com>
2019-01-13 13:20:32 -0300

    Blank line (PEP8) lol

--- wedding_gallery/config.py
+++ wedding_gallery/config.py
@@ -2,6 +2,7 @@ import os
 
 HOME = os.path.expanduser('~')
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = os.environ.get('FLASK_DEBUG') or False
