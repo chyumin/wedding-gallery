98b6b09d1d3b88a63c08a05d53791bc8cb6b4982
Chuang Yu Min <chg.yumin@gmail.com>
2019-01-13 15:46:37 -0300

    Changed Text to String

--- wedding_gallery/models/core.py
+++ wedding_gallery/models/core.py
@@ -6,9 +6,9 @@ from wedding_gallery.models import db
 class GalleryUser(db.Model):
     __tablename__ = 'gallery_user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text(64), nullable=False, unique=True)
-    name = db.Column(db.Text)
-    password = db.Column(db.Text(128), nullable=False)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    name = db.Column(db.String)
+    password = db.Column(db.String(128), nullable=False)
     master = db.Column(db.Boolean, nullable=False, server_default='0')
 
 
