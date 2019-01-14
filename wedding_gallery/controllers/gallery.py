a33c776b262952375e25eead6ddb6ea646caad47
Chuang Yu Min <chg.yumin@gmail.com>
2019-01-14 01:14:43 -0300

    Added Approve photos

--- wedding_gallery/controllers/gallery.py
+++ wedding_gallery/controllers/gallery.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import flash, redirect, render_template, request, url_for
 
 from wedding_gallery import app
-from wedding_gallery.models import core
+from wedding_gallery.models import core, DBSession
 
 
 @app.route('/')
@@ -12,3 +12,27 @@ def index():
         'title': 'Wedding Gallery'
     }
     return render_template('index.html', **template_args)
+
+
+@app.route('/approve_photos')
+def approve_photos():
+    photos = core.Photo.query.filter_by(approved=False)
+    template_args = {
+        'photos': photos,
+        'title': 'Approve Photos'
+    }
+    return render_template('approve_photos.html', **template_args)
+
+
+@app.route('/do_approve', methods=['POST'])
+def do_approve():
+    photos_ids_list = request.form.getlist('checkbox-list')
+    query = DBSession.query(core.Photo).filter(
+        core.Photo.id.in_(photos_ids_list)
+    )
+    query.update({
+        core.Photo.approved: True
+    }, synchronize_session=False)
+    DBSession.commit()
+    flash('Photos Approved!')
+    return redirect(url_for('approve_photos'))
