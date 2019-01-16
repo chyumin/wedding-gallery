146e6d072a70b0036ed7143060c03c9e0c74e9f9
Bruno Dias <dias.h.bruno@gmail.com>
2019-01-16 00:21:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
+
 setup(
     name='wedding_gallery',
+    version='0.0.1',
     packages=['wedding_gallery'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
+    install_requires=reqs
 )
